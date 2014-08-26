cdb9d5e86c1ab1f9d76d656a9787f1e5ab6a7db9
aaaddress1 <aaaddress1@gmail.com>
2014-08-27 00:51:50 +0800

    聊聊剃除 By 馬聖豪

---
+++ FkBBTalk/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "全自動剔除聊聊"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Button1.Location = New System.Drawing.Point(0, 44)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(339, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "結束本程序"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(339, 44)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(339, 67)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "全自動剃除聊聊 By.aaaddress1@gmail.com"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ FkBBTalk/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    Public Declare Function WriteProcessMemory Lib "kernel32" (ByVal hProcess As Integer, ByVal lpBaseAddress As Integer, ByVal lpBuffer As Byte(), ByVal nSize As Integer, ByRef lpNumberOfBytesWritten As Integer) As Integer
+    Public Declare Function GetProcAddress Lib "kernel32" (ByVal hModule As Integer, ByVal lpProcName As String) As Integer
+    Private Declare Function GetModuleHandle Lib "Kernel32" Alias "GetModuleHandleA" (ByVal lpModuleName As String) As Integer
+
+    Sub mFunction()
+        On Error Resume Next
+        While True
+            If (Process.GetProcessesByName("BBTalk").Length = 0) Then
+                Label1.Text = "尚未發現該死的聊聊."
+            Else
+                If ((Process.GetProcessesByName("lol").Length <> 0)) And ((Process.GetProcessesByName("LolClient").Length <> 0)) Then
+
+                    Label1.Text = "開始處理基巴聊聊吧 OwO"
+                    Process.GetProcessesByName("BBTalk")(0).Kill()
+                    Dim TargetHandle As IntPtr = Process.GetProcessesByName("ggdllhost")(0).Handle
+                    Dim AdrOfBP As IntPtr = GetProcAddress(GetModuleHandle("ntdll"), "DbgBreakPoint")
+                    Dim AdrOfRB As IntPtr = GetProcAddress(GetModuleHandle("ntdll"), "DbgUiRemoteBreakin")
+                    Dim AdrOfUB As IntPtr = GetProcAddress(GetModuleHandle("ntdll"), "DbgUserBreakPoint")
+                    WriteProcessMemory(TargetHandle, AdrOfBP, {&HCC}, 1, 0)
+                    WriteProcessMemory(TargetHandle, AdrOfRB, {&H6A, &H8, &H68, &HE8, &H7}, 5, 0)
+                    WriteProcessMemory(TargetHandle, AdrOfUB, {&HCC, &H90, &HC3, &H90, &HCC}, 5, 0)
+                    Label1.Text = "強姦聊聊完畢 OwO"
+
+                Else
+                    Label1.Text = "快開啟LOL阿你..."
+                End If
+            End If
+            Threading.Thread.Sleep(3000)
+        End While
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        e.Cancel = True
+        Visible = False
+        If e.CloseReason = CloseReason.WindowsShutDown Then
+            End
+        ElseIf (Visible = False) Then
+            NotifyIcon1.BalloonTipTitle = "縮小"
+            NotifyIcon1.BalloonTipText = "已於工具列隱藏"
+            NotifyIcon1.ShowBalloonTip(500)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Form.CheckForIllegalCrossThreadCalls = False
+        Dim Thread As New Threading.Thread(AddressOf mFunction)
+        Thread.Start()
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Visible = Not Visible
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
+End Class
